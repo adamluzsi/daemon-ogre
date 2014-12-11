701e2ed6d370dc23fc10084e9828c04bcad49226
Adam Luzsi <adamluzsi@gmail.com>
2014-12-11 21:25:39 +0100

    upd argv

--- lib/daemon-ogre/argv.rb
+++ lib/daemon-ogre/argv.rb
@@ -5,7 +5,7 @@ module DaemonOgre
 
       def check_args_for( *args )
         args = args.map(&:to_s)
-        return ::ARGV.any?{|argument| args.any?{|string_to_find| argument =~ Regexp.new(string_to_find.to_s)  }}
+        return ::ARGV.any?{|argument| args.any?{|string_to_find| argument =~ /--?#{string_to_find.to_s}$/ }}
       end
 
       @@daemon_keys= [:daemonize,:daemon,:d]
